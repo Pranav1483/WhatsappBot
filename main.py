da383a5aea6346cc10449ac224bab136d75f3375
Pranav P <pranavp1483@gmail.com>
2022-09-13 18:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import pyautogui as pg
-import webbrowser as web
-import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
-data = pd.read_csv("File_Name.csv")
-data_dict = data.to_dict('list')
-leads = data_dict['Number']
-messages = data_dict['Message']
-combo = zip(leads, messages)
-first = True
-for lead, message in combo:
-    time.sleep(4)
-    web.open("https://web.whatsapp.com/send?phone="+lead+"&text="+message)
-    time.sleep(6)
-    width, height = pg.size()
-    pg.click(width/2, height/1.11)
-    time.sleep(8)
-    pg.press('enter')
-    time.sleep(8)
-    pg.hotkey('ctrl', 'w')
+from selenium.webdriver.support.wait import WebDriverWait as Wait
+import selenium.webdriver.support.expected_conditions as ec
+import os
+
+
+def chat(lead, message):
+    options = webdriver.ChromeOptions()
+    options.add_argument('user-data-dir='+os.getcwd())
+    browser = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+    browser.get('https://web.whatsapp.com/send?phone='+str(lead)+'&text='+message)
+    Wait(browser, 50).until(ec.any_of(ec.presence_of_element_located(('class name', '_2UwZ_')), ec.presence_of_element_located(('xpath', '(.//div[@class = "fd365im1 to2l77zo bbv8nyr4 mwp4sxku gfz4du6o ag5g9lrv"])'))))
+    if ec.presence_of_element_located(('class name', '_2UwZ_')):
+        Wait(browser, 20).until_not(ec.presence_of_element_located(('class name', '_2UwZ_')))
+    Wait(browser, 20).until(ec.presence_of_element_located(('xpath', '(.//div[@class = "fd365im1 to2l77zo bbv8nyr4 mwp4sxku gfz4du6o ag5g9lrv"])')))
+    browser.find_element('xpath', '(.//div[@class = "fd365im1 to2l77zo bbv8nyr4 mwp4sxku gfz4du6o ag5g9lrv"])').send_keys(Keys.ENTER)
+    Wait(browser, 20).until_not(ec.presence_of_element_located(('xpath', '(.//span[@data-testid = "msg-time"])')))
+    browser.quit()
+
+
+df = pd.read_csv('contact.csv')
+content = df.to_dict('list')
+combo = zip(content['Number'], content['Message'])
+for l, m in combo:
+    chat(l, m)
